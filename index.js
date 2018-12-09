94ef1122d2858dece5fa93a69c85031b05fa6d8d
cazahec <cazahec@gmail.com>
2018-12-09 13:50:48 -0600

    primer inteno

---
+++ index.js
@@ -0,0 +1,13 @@
+const {BrowswerWindow}=require('electron').remote
+const app=require('electron').app
+const path=require('path')
+const url=require('url')
+
+var vitrina=""
+
+var mostrarvitrinas=function(){
+	var url='http://museobillete.azurewebsites.net/api/Expo'
+	
+
+}
+
